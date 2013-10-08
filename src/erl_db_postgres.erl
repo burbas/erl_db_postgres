badfb199adfd13acb3510a5704b8bf15dafbbcde
Niclas Axelsson <niclas@burbas.se>
2013-10-08 12:30:49 +0200

    Fixed the find- and save-functions. Updated the code to use the new representation of fields.

--- src/erl_db_postgres.erl
+++ src/erl_db_postgres.erl
@@ -1,4 +1,12 @@
+%%%-------------------------------------------------------------------
+%%% @author Niclas Axelsson <niclas@burbasconsulting.com>
+%%% @doc
+%%% ErlDB handler for postgres
+%%% @end
+%%%-------------------------------------------------------------------
 -module(erl_db_postgres).
+-author('Niclas Axelsson <niclas@burbasconsulting.com>').
+
 -behaviour(gen_server).
 -behaviour(poolboy_worker).
 
@@ -9,30 +17,64 @@
          code_change/3]).
 
 %% Only for testing
--export([build_insert_query/1, build_update_query/1, build_table/1]).
+%%-export([build_insert_query/1, build_update_query/1, build_table/1, build_select_query/2]).
 
 -record(state, {
-          conn
+          name :: atom(),
+          type :: atom(),
+          conn :: pid()
          }).
 
+%%%===================================================================
+%%% API
+%%%===================================================================
 
-%%%%%% API %%%%%%%%
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
 start_link(Args) ->
-    gen_server:start_link(?MODULE, Args, []).
+    WorkerArgs = proplists:get_value(worker_args, Args),
+    gen_server:start_link(?MODULE, WorkerArgs, []).
 
-%%%%%% Internals %%%%%%%%
 
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
 init(Args) ->
+    DBName = proplists:get_value(db_name, Args),
+    DBType = proplists:get_value(db_type, Args),
     Hostname = proplists:get_value(hostname, Args),
     Database = proplists:get_value(database, Args),
     Username = proplists:get_value(username, Args),
     Password = proplists:get_value(password, Args),
 
-    erl_db_log:msg(debug, "~p started.", [?MODULE]),
+    erl_db_log:msg(debug, "~p started with args: ~p.", [?MODULE, [Args]]),
+
     {ok, Conn} = pgsql:connect(Hostname, Username, Password, [
         {database, Database}
     ]),
-    {ok, #state{conn=Conn}}.
+
+    {ok, #state{
+       name = DBName,
+       type = DBType,
+       conn = Conn}}.
 
 
 handle_call({refresh, Model}, _From, #state{conn=Conn}=State) ->
@@ -45,27 +87,32 @@ handle_call({refresh, Model}, _From, #state{conn=Conn}=State) ->
     {reply, ok, State};
 
 handle_call({delete, Model}, _From, #state{conn=Conn}=State) ->
-
     {reply, ok, State};
 
-handle_call({save, Model}, _From, #state{conn=Conn}=State) ->
-    Table = erlang:element(1, Model),
-    ModelAttributes = Table:module_info(attributes),
-    Fields = proplists:get_value(fields, ModelAttributes),
+handle_call({save, Model}, _From, #state{conn=Conn, name = DBName}=State) ->
+    Modelname = element(1, Model),
+    Table = get_table(Modelname, DBName),
+
+    %% Create the table if it does not exist
+    [ build_table(Modelname, Table, Conn) || not table_exist(Table, Conn) ],
+
+    Fields = erl_db_utils:get_fields(Modelname),
+
     Query =
-        case Model:id() of
-            'id' ->
+        case erl_db_utils:is_update_query(Model) of
+            false ->
                 build_insert_query(Model);
             _ ->
                 build_update_query(Model)
         end,
+
     Result = pgsql:equery(Conn, Query, []),
+
     Reply =
         case Result of
             {ok, _, _, [{Id}|_Tl]} ->
-                %% Get the primary key
-                [PrimKey|_] = get_fields_with_type(primary_key, Fields),
-                {ok, Model:PrimKey(Id)};
+                %% We should set this to something useful
+                {ok, erl_db_utils:set_primary_key(Model, Id)};
             {ok, _Id} ->
                 {ok, Model};
             {error, Reason} ->
@@ -73,21 +120,17 @@ handle_call({save, Model}, _From, #state{conn=Conn}=State) ->
         end,
     {reply, Reply, State};
 
-handle_call({find, Conditions}, _From, #state{conn=Conn}=State) ->
-    {reply, ok, State};
+handle_call({find, Tablename, Conditions, _Options}, _From, #state{conn=Conn}=State) ->
+    %% This is a bit tricky since we need to know which table this belongs to
+    Query = build_select_query(Tablename, Conditions),
+    {ok, Columns, Results} = pgsql:squery(Conn, lists:concat(Query)),
+    %% We're getting correct results, but we need to model this after our model.
+    Models = merge_result(Tablename, Columns, Results),
+    {reply, {ok, Models}, State};
 
-handle_call({create_table, Model}, _From, #state{conn=Conn}=State) ->
-    Reply =
-        case table_exist(Model, Conn) of
-            true ->
-                erl_db_log:msg(warning, "Table ~p does already exist", [Model]),
-                {error, table_already_exist};
-            _ ->
-                Query = build_table(Model),
-                {ok, _, _} = pgsql:squery(Conn, Query),
-                {ok, Model}
-        end,
-    {reply, Reply, State};
+
+handle_call({supported_conditions}, _From, State) ->
+    {reply, {ok, ['equals', 'not_equals']}, State};
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
@@ -106,9 +149,24 @@ code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 
-table_exist(Model, Conn) when is_atom(Model) ->
+
+get_table(Modelname, DBName) ->
+    Backends = proplists:get_value(backend, Modelname:module_info(attributes), []),
+    case proplists:get_value(DBName, Backends) of
+        undefined ->
+            throw({error, model_error});
+        BackendArgs ->
+            case proplists:get_value(table, BackendArgs) of
+                undefined ->
+                    Modelname;
+                Table ->
+                    Table
+            end
+    end.
+
+table_exist(Table, Conn) when is_atom(Table) ->
     Query = "SELECT * FROM information_schema.tables WHERE table_name=$1",
-    case pgsql:equery(Conn, Query, [Model]) of
+    case pgsql:equery(Conn, Query, [Table]) of
         {ok, _Columns, []} ->
             false;
         {ok, _Columns, _Field} ->
@@ -117,13 +175,13 @@ table_exist(Model, Conn) when is_atom(Model) ->
             false
     end.
 
-build_table(Modelname) when is_atom(Modelname) ->
+build_table(Modelname, Table, Conn) when is_atom(Modelname) ->
     ModelAttributes = Modelname:module_info(attributes),
     Fields = proplists:get_value(fields, ModelAttributes),
 
     Attributes =
         lists:foldl(
-          fun({Field, Type, Args}, Acc) ->
+          fun({Field, Type, Args, _Index}, Acc) ->
                   case Type of
                       string ->
                           Val =
@@ -179,11 +237,15 @@ build_table(Modelname) when is_atom(Modelname) ->
                   end
           end, [], lists:reverse(Fields)),
 
-    ["CREATE TABLE ", atom_to_list(Modelname), " (",
-     string:join(Attributes, ","), ")"].
+    Query =
+        ["CREATE TABLE ", atom_to_list(Table), " (",
+         string:join(Attributes, ","), ")"],
+    pgsql:equery(Conn, Query, []).
 
+build_select_query(Tablename, []) ->
+    ["SELECT * FROM ", Tablename];
 build_select_query(Tablename, Fields) ->
-    ok.
+    ["SELECT * FROM ", Tablename, " WHERE ", build_conditions(Fields)].
 
 build_update_query(Model) ->
     Modelname = element(1, Model),
@@ -192,10 +254,10 @@ build_update_query(Model) ->
     Fields = proplists:get_value(fields, ModelAttributes),
     {Wheres, Values} =
         lists:foldl(
-          fun({Field, primary_key, _Args}, {_, Vals}) ->
-                  {[atom_to_list(Field), "=", pack_value(Model:Field())], Vals};
-             ({Field, _Type, _Args}, {Where, Vals}) ->
-                  {Where, [lists:concat([atom_to_list(Field) ++ "=" ++ pack_value(Model:Field())])|Vals]}
+          fun({Field, primary_key, _Args, Index}, {_, Vals}) ->
+                  {[atom_to_list(Field), "=", pack_value(element(Index, Model))], Vals};
+             ({Field, _Type, _Args, Index}, {Where, Vals}) ->
+                  {Where, [lists:concat([atom_to_list(Field) ++ "=" ++ pack_value(element(Index, Model))])|Vals]}
           end,
           {[], []}, Fields),
     ["UPDATE ", Tablename, " SET ",
@@ -210,15 +272,15 @@ build_insert_query(Model) ->
     Fields = proplists:get_value(fields, ModelAttributes),
     {Attributes, Values} =
         lists:foldl(
-          fun({Field, primary_key, Args}, {Attrs, Vals}=Acc) ->
+          fun({Field, primary_key, Args, Index}, {Attrs, Vals}=Acc) ->
                   case proplists:get_value(auto_increment, Args) of
                       true ->
                           Acc;
                       false ->
-                          {[atom_to_list(Field)|Attrs], [pack_value(Model:Field())|Vals]}
+                          {[atom_to_list(Field)|Attrs], [pack_value(element(Index, Model))|Vals]}
                   end;
-             ({Field, _Type, _Args}, {Attrs, Vals}) ->
-                  {[atom_to_list(Field)|Attrs], [pack_value(Model:Field())|Vals]}
+             ({Field, _Type, _Args, Index}, {Attrs, Vals}) ->
+                  {[atom_to_list(Field)|Attrs], [pack_value(element(Index, Model))|Vals]}
           end,
           {[], []}, Fields),
     ["INSERT INTO ", Tablename, " (",
@@ -228,14 +290,35 @@ build_insert_query(Model) ->
      ")",
      " RETURNING id"].
 
+merge_result(Modelname, Columns, Results) ->
+    Merge = [ merge_result(Columns, tuple_to_list(X)) || X <- Results ],
+    Model = Modelname:dummy(),
+    Models = [ build_model(Model, Data) || Data <- Merge ].
 
 
-get_fields_with_type(_Type, []) ->
+build_model(Model, []) -> Model;
+build_model(Model, [{Fieldname, Data}|Tl]) ->
+    build_model(erl_db_utils:set_field(Model, Fieldname, Data), Tl).
+
+merge_result([], []) -> [];
+merge_result([{column, Fieldname, Type, _, _, _}|ColTl], [Res|Results]) ->
+    [{binary_to_atom(Fieldname, utf8), unpack_value(Res, Type)}|merge_result(ColTl, Results)].
+
+
+condition(Key, Op, Val) ->
+    lists:concat([Key, Op, pack_value(Val)]).
+
+build_conditions(Conditions) ->
+    ConditionLst = build_conditions1(Conditions),
+    string:join(ConditionLst, " AND ").
+
+build_conditions1([]) ->
     [];
-get_fields_with_type(Type, [{Name, Type, _}|Tl]) ->
-    [Name|get_fields_with_type(Type, Tl)];
-get_fields_with_type(Type, [_Hd|Tl]) ->
-    get_fields_with_type(Type, Tl).
+build_conditions1([{Key, 'equals', Value}|Tl]) ->
+    [condition(Key, "=", Value)|build_conditions1(Tl)];
+build_conditions1([{Key, 'not_equals', Value}|Tl]) ->
+    [condition(Key, "!=", Value)|build_conditions1(Tl)].
+
 
 escape_sql(Value) ->
     escape_sql1(Value, []).
@@ -273,3 +356,10 @@ pack_value(true) ->
     "TRUE";
 pack_value(false) ->
     "FALSE".
+
+
+unpack_value(Value, int4) when is_binary(Value) ->
+    binary_to_integer(Value);
+unpack_value(Value, varchar) when is_binary(Value) ->
+    binary_to_list(Value).
+
